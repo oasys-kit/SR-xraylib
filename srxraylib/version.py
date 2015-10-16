dfdb546f46f63a47b422d2035a92c97110a256db
luca.rebuffi@elettra.eu <Bacilotto_00>
2015-10-16 13:24:21 +0200

    management of the wrking directories

---
+++ srxraylib/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM OASYS SETUP.PY
+short_version = '0.0.1'
+version = '0.0.1'
+full_version = '0.0.1.dev0+3f0a696'
+git_revision = '3f0a696b7f5a2e7afa4bc0e99b7946de1cf2e03f'
+release = False
+
+if not release:
+    version = full_version
+    short_version += ".dev"
