1db527fd3c3056787a729128bab9855e4df3f4cd
luca.rebuffi@elettra.eu <Bacilotto_00>
2015-12-09 15:57:04 +0100

    New wave optics library

---
+++ srxraylib/waveoptics/propagator.py
@@ -0,0 +1,43 @@
+import numpy
+
+from srxraylib.util.data_structures import ScaledArray
+from srxraylib.waveoptics.wavefront import Wavefront1D
+
+def propagate_1D_fresnel(wavefront, propagation_distance):
+    fft_scale = numpy.fft.fftfreq(wavefront.size())/wavefront.delta()
+
+    fft = numpy.fft.fft(wavefront.get_complex_amplitude())
+    fft *= numpy.exp((-1.0j) * numpy.pi * wavefront.get_wavelenght() * propagation_distance * fft_scale**2)
+    ifft = numpy.fft.ifft(fft)
+
+    return Wavefront1D(wavefront.get_wavelenght(), ScaledArray.initialize_from_steps(ifft, wavefront.offset(), wavefront.delta()))
+
+
+if __name__ == "__main__":
+    wavelength = 1.24e-10 # 10keV
+    aperture_diameter = 40e-6 # 1e-3 # 1e-6
+    detector_size = 800e-6
+    distance = 3.6
+    npoints = 1000
+
+    wavefront = Wavefront1D.initialize_wavefront_from_range(wavelenght=wavelength, number_of_points=npoints, x_min=-detector_size/2, x_max=detector_size/2)
+    wavefront.set_plane_wave_from_complex_amplitude((2.0+1.0j))
+    wavefront.apply_slit(-aperture_diameter/2, aperture_diameter/2)
+
+    wavefront_2 = propagate_1D_fresnel(wavefront, distance)
+
+    wavefront.apply_ideal_lens(distance)
+    wavefront_3 = propagate_1D_fresnel(wavefront, distance)
+
+
+    import matplotlib.pylab as plt
+    f1 = plt.figure(1)
+    plt.plot(wavefront_2.get_abscissas()*1e6, wavefront_2.get_intensity())
+    plt.plot(wavefront_3.get_abscissas()*1e6, wavefront_3.get_intensity())
+    plt.title("Manolo for President")
+    plt.xlabel("X (m)")
+    plt.ylabel("I V^2 m^-2")
+    plt.xlim([-60, 60])
+    plt.show()
+
+
---
+++ srxraylib/waveoptics/wavefront.py
@@ -0,0 +1,137 @@
+
+import numpy
+from srxraylib.util.data_structures import ScaledArray, ScaledMatrix
+
+#------------------------------------------------
+#
+#
+#
+#
+#------------------------------------------------
+
+class Wavefront1D(object):
+    wavelenght = 0.0
+    electric_field_array = None
+
+    def __init__(self, wavelenght=1e-10, electric_field_array=None):
+        self.wavelenght = wavelenght
+        self.electric_field_array = electric_field_array
+
+    @classmethod
+    def initialize_wavefront(cls, wavelenght=1e-10, number_of_points=1000):
+        return Wavefront1D(wavelenght, ScaledArray.initialize(np_array=numpy.full(number_of_points, (1.0 + 0.0j), dtype=complex)))
+
+    @classmethod
+    def initialize_wavefront_from_steps(cls, wavelenght=1e-10, number_of_points=1000, x_start=0.0, x_step=0.0):
+        return Wavefront1D(wavelenght, ScaledArray.initialize_from_steps(np_array=numpy.full(number_of_points, (1.0 + 0.0j), dtype=complex),
+                                                                         initial_scale_value=x_start,
+                                                                         scale_step=x_step))
+    @classmethod
+    def initialize_wavefront_from_range(cls, wavelenght=1e-10, number_of_points=1000, x_min=0.0, x_max=0.0):
+        return Wavefront1D(wavelenght, ScaledArray.initialize_from_range(np_array=numpy.full(number_of_points, (1.0 + 0.0j), dtype=complex),
+                                                                         min_scale_value=x_min,
+                                                                         max_scale_value=x_max))
+    def size(self):
+        return self.electric_field_array.size()
+
+    def delta(self):
+        return self.electric_field_array.delta()
+
+    def offset(self):
+        return self.electric_field_array.offset()
+
+    def get_wavelenght(self):
+        return self.wavelenght
+
+    def get_wavenumber(self):
+        return 2*numpy.pi/self.wavelenght
+
+    def get_abscissas(self):
+        return self.electric_field_array.scale
+
+    def get_complex_amplitude(self):
+        return self.electric_field_array.np_array
+
+    def get_amplitude(self):
+        return numpy.absolute(self.get_complex_amplitude())
+
+    def get_phase(self):
+        return numpy.arctan2(numpy.imag(self.get_complex_amplitude()), numpy.real(self.get_complex_amplitude()))
+
+    def get_intensity(self):
+        return self.get_amplitude()**2
+
+    def get_complex_amplitude_from_abscissa(self, abscissa_value):
+        return self.electric_field_array.interpolate_value(abscissa_value)
+
+    def get_amplitude_from_abscissa(self, abscissa_value):
+        return numpy.absolute(self.get_complex_amplitude_from_abscissa(abscissa_value))
+
+    def get_phase_from_abscissa(self, abscissa_value):
+        complex_amplitude = self.get_complex_amplitude_from_abscissa(abscissa_value)
+        return numpy.arctan2(numpy.imag(complex_amplitude), numpy.real(complex_amplitude))
+
+    def get_intensity_from_abscissa(self, abscissa_value):
+        return self.get_amplitude_from_abscissa(abscissa_value)**2
+
+    def set_plane_wave_from_complex_amplitude(self, complex_amplitude=(1.0 + 0.0j)):
+        self.electric_field_array.np_array = numpy.full(self.electric_field_array.size(), complex_amplitude, dtype=complex)
+
+    def set_plane_wave_from_amplitude_and_phase(self, amplitude=1.0, phase=0.0):
+        self.set_plane_wave_from_complex_amplitude(amplitude*numpy.cos(phase) + 1.0j*amplitude*numpy.sin(phase))
+
+    def set_spherical_wave(self, amplitude=1.0, radius=1.0):
+        if radius == 0: raise Exception("Radius cannot be zero")
+        self.electric_field_array.np_array = (amplitude/radius)*numpy.exp(-1.0j*self.get_wavenumber()*(self.electric_field_array.scale**2)/(2*radius))
+
+    def add_phase_shift(self, phase_shift):
+        self.electric_field_array.np_array *= numpy.exp(1.0j*phase_shift)
+
+    def add_phase_shifts(self, phase_shifts):
+        if phase_shifts.size != self.electric_field_array.size(): raise Exception("Phase Shifts array has different dimension")
+        self.electric_field_array.np_array =  numpy.multiply(self.electric_field_array.np_array, numpy.exp(1.0j*phase_shifts))
+
+    def rescale_amplitude(self, factor):
+        self.electric_field_array.np_array *= factor
+
+    def rescale_amplitudes(self, factors):
+        if factors.size != self.electric_field_array.size(): raise Exception("Factors array has different dimension")
+        self.electric_field_array.np_array =  numpy.multiply(self.electric_field_array.np_array, factors)
+
+    def apply_ideal_lens(self, focal_length):
+        self.add_phase_shift((-1.0) * self.get_wavenumber() * (self.get_abscissas() ** 2 / focal_length) / 2)
+
+    def apply_slit(self, x_slit_min, x_slit_max):
+        window = numpy.ones(self.electric_field_array.size())
+
+        lower_window = numpy.where(self.get_abscissas() < x_slit_min)
+        upper_window = numpy.where(self.get_abscissas() > x_slit_max)
+
+        if len(lower_window) > 0: window[lower_window] = 0
+        if len(upper_window) > 0: window[upper_window] = 0
+
+        self.rescale_amplitudes(window)
+
+if __name__ == "__main__":
+
+    wavefront = Wavefront1D.initialize_wavefront_from_range(wavelenght=1e-2, number_of_points=1000000, x_min=-1, x_max=1)
+    wavefront.set_plane_wave_from_amplitude_and_phase(2, 1.0)
+    #wavefront.apply_ideal_lens(100)
+    wavefront.apply_slit(-0.4, 0.4)
+
+    import matplotlib.pylab as plt
+    f1 = plt.figure(1)
+    plt.plot(wavefront.get_abscissas(), numpy.degrees(wavefront.get_intensity()))
+    plt.title("Manolo for President")
+    plt.xlabel("X (m)")
+    plt.ylabel("I V^2 m^-2")
+    plt.show()
+
+
+
+
+
+
+
+
+
