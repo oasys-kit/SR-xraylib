839bd6ff1c1b632139fabdb5d416cbe1c25b1b27
Luca Rebuffi <luca.rebuffi@elettra.eu>
2015-12-15 13:01:14 +0100

    Bug fixing

--- srxraylib/version.py
+++ srxraylib/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
 short_version = '0.0.9'
 version = '0.0.9'
-full_version = '0.0.9.dev0+eaa3f27'
-git_revision = 'eaa3f27a4f0fd9b4e470f033bddfa8de40633d80'
+full_version = '0.0.9.dev0+a80bf1b'
+git_revision = 'a80bf1befea0276889b0fa8c1fdcbc8bda48ae18'
 release = False
 
 if not release:
