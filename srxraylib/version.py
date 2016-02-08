e93cbfcc4a031bafa1d33621ca33182baa355dc7
luca.rebuffi@elettra.eu <Bacilotto_00>
2016-02-08 22:17:48 +0100

    Issue #90

--- srxraylib/version.py
+++ srxraylib/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
 short_version = '1.0.0'
 version = '1.0.0'
-full_version = '1.0.0.dev0+c3705f3'
-git_revision = 'c3705f3419177192d3ffd65993a113bc1a13bb44'
+full_version = '1.0.0.dev0+87b814e'
+git_revision = '87b814e8ce36972b262caebabfb82db85d47d996'
 release = False
 
 if not release:
