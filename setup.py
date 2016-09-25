f50d0eea986ee01e8c7cffcbb28a20ffb485f5d0
Luca Rebuffi <luca.rebuffi@elettra.eu>
2016-09-25 16:14:06 +0200

    better setup.py (srxraylib last version)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 NAME = 'srxraylib'
 
-VERSION = '1.0.7'
+VERSION = '1.0.8'
 ISRELEASED = False
 
 DESCRIPTION = 'Synchrotron Radiation X-ray library'
--- srxraylib/version.py
+++ srxraylib/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
-short_version = '1.0.7'
-version = '1.0.7'
-full_version = '1.0.7.dev0+19b8146'
-git_revision = '19b81463c47e52fd501176d39f302349bd3da107'
+short_version = '1.0.8'
+version = '1.0.8'
+full_version = '1.0.8.dev0+ae234eb'
+git_revision = 'ae234eb1e17bbbd2d84f073c3539d4cf8cd210c5'
 release = False
 
 if not release:
