a939d77af013c55b3c73eb7035fad71c5ba9cfa8
Luca Rebuffi <luca.rebuffi@elettra.eu>
2016-12-19 17:43:06 +0100

    Corrected error on Fractal profiles simulations.

--- srxraylib/version.py
+++ srxraylib/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
-short_version = '1.0.8'
-version = '1.0.8'
-full_version = '1.0.8.dev0+f50d0ee'
-git_revision = 'f50d0eea986ee01e8c7cffcbb28a20ffb485f5d0'
+short_version = '1.0.9'
+version = '1.0.9'
+full_version = '1.0.9.dev0+53b30ae'
+git_revision = '53b30ae5dd0c5da33ad22e73d793756193ac1ffe'
 release = False
 
 if not release:
