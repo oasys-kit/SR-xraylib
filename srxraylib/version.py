9d28d90c303786ac1b7c771a3cf49985f4de8c5a
luca.rebuffi@elettra.eu <Bacilotto_00>
2017-01-27 23:11:15 +0100

    Migration of Waveoptics library from SR-Xraylib

--- srxraylib/version.py
+++ srxraylib/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
-short_version = '1.0.9'
-version = '1.0.9'
-full_version = '1.0.9.dev0+53b30ae'
-git_revision = '53b30ae5dd0c5da33ad22e73d793756193ac1ffe'
+short_version = '1.0.10'
+version = '1.0.10'
+full_version = '1.0.10.dev0+e590058'
+git_revision = 'e590058aeecd6ace3b7d4e5138b03233db8a1f71'
 release = False
 
 if not release:
