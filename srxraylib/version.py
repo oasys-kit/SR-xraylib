f24cd0a00720173d5a0fcb5cde0ff99e1c4fb145
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-05-17 17:43:35 +0200

    Oasys 1.0 Release

--- srxraylib/version.py
+++ srxraylib/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
-short_version = '1.0.13'
-version = '1.0.13'
-full_version = '1.0.13.dev0+cd289cd'
-git_revision = 'cd289cd7ca87b3ef8baaebe319085f1de5407e81'
+short_version = '1.0.14'
+version = '1.0.14'
+full_version = '1.0.14.dev0+609761a'
+git_revision = '609761a247686ab4a866b6011ebe78db9662ae8e'
 release = False
 
 if not release:
