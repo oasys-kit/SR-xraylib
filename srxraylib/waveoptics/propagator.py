1c5a6c605acdf86ffcfb3a3b7094ddb3bd6dc072
Manuel Sanchez del Rio <srio@esrf.eu>
2017-11-22 10:12:47 +0100

    1d integral propagator by loop on detector coordinates

--- srxraylib/waveoptics/propagator.py
+++ srxraylib/waveoptics/propagator.py
@@ -72,28 +72,37 @@ def propagate_1D_fresnel_convolution(wavefront, propagation_distance):
     return Wavefront1D(wavefront.get_wavelength(), ScaledArray.initialize_from_steps(tmp, wavefront.offset(), wavefront.delta()))
 
 
-def propagate_1D_integral(wavefront, propagation_distance, detector_abscissas=[None]):
+def propagate_1D_integral(wavefront, propagation_distance, detector_abscissas=[None], method=0):
     """
     1D Fresnel-Kirchhoff propagator via simplified integral
     :param wavefront:
     :param propagation_distance: propagation distance
     :param detector_abscissas: a numpy array with the anscissas at the image position. If undefined ([None])
                             it uses the same abscissas present in input wavefront.
+    :param method: 0 (default_ makes a loop over detector coordinates, 1: makes matrices (outer products) so
+                            it is more memory hungry.
     :return: a new 1D wavefront object with propagated wavefront
     """
 
     if detector_abscissas[0] == None:
         detector_abscissas = wavefront.get_abscissas()
 
-    # calculate via outer product, it spreads over a lot of memory, but it is OK for 1D
-    x1 = numpy.outer(wavefront.get_abscissas(),numpy.ones(detector_abscissas.size))
-    x2 = numpy.outer(numpy.ones(wavefront.size()),detector_abscissas)
-    r = numpy.sqrt( numpy.power(x1-x2,2) + numpy.power(propagation_distance,2) )
     wavenumber = numpy.pi*2/wavefront.get_wavelength()
-    distances_matrix  = numpy.exp(1.j * wavenumber *  r)
-
-
-    fieldComplexAmplitude = numpy.dot(wavefront.get_complex_amplitude(),distances_matrix)
+    if method == 0:
+        x1 = wavefront.get_abscissas()
+        x2 = detector_abscissas
+        fieldComplexAmplitude = numpy.zeros_like(x2,dtype=complex)
+        for ix,x in enumerate(x2):
+            r = numpy.sqrt( numpy.power(x1-x,2) + numpy.power(propagation_distance,2) )
+            distances_array  = numpy.exp(1.j * wavenumber *  r)
+            fieldComplexAmplitude[ix] = (wavefront.get_complex_amplitude() * distances_array).sum()
+    elif method==1:
+        # calculate via outer product, it spreads over a lot of memory, but it is OK for 1D
+        x1 = numpy.outer(wavefront.get_abscissas(),numpy.ones(detector_abscissas.size))
+        x2 = numpy.outer(numpy.ones(wavefront.size()),detector_abscissas)
+        r = numpy.sqrt( numpy.power(x1-x2,2) + numpy.power(propagation_distance,2) )
+        distances_matrix  = numpy.exp(1.j * wavenumber *  r)
+        fieldComplexAmplitude = numpy.dot(wavefront.get_complex_amplitude(),distances_matrix)
 
     return Wavefront1D(wavefront.get_wavelength(), ScaledArray.initialize_from_steps(fieldComplexAmplitude, \
                             detector_abscissas[0], detector_abscissas[1]-detector_abscissas[0] ))
