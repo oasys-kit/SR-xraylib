b26c019c29b7a3d329e9e0fe1330fbe98cf279ad
lucarebuffi <luca.rebuffi@elettra.eu>
2018-09-18 10:16:22 -0500

    new APS ShadowOui customized widgets

--- srxraylib/version.py
+++ srxraylib/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
 short_version = '1.0.19'
 version = '1.0.19'
-full_version = '1.0.19.dev0+0e64807'
-git_revision = '0e648079ac3a55086947492fdd5067e923ca07a8'
+full_version = '1.0.19.dev0+298bdfd'
+git_revision = '298bdfdf963954e4271be0a6c32cab0de67718b3'
 release = False
 
 if not release:
