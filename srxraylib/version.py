f39736b6f8fa5407377a78abd5f7a10fe830f4cc
lucarebuffi <lrebuffi@anl.gov>
2018-10-08 11:12:04 -0500

    SRW CRL

--- srxraylib/version.py
+++ srxraylib/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
 short_version = '1.0.19'
 version = '1.0.19'
-full_version = '1.0.19.dev0+298bdfd'
-git_revision = '298bdfdf963954e4271be0a6c32cab0de67718b3'
+full_version = '1.0.19.dev0+c0bef7b'
+git_revision = 'c0bef7b4e8bb7b4f73e0d3f0f1af739d464a001a'
 release = False
 
 if not release:
