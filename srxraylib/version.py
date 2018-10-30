fdbfed9bf464a5abafd03967f6d8da3b51b235dd
lucarebuffi <lrebuffi@anl.gov>
2018-10-30 17:02:34 -0500

    Full Undulator!

--- srxraylib/version.py
+++ srxraylib/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
-short_version = '1.0.19'
-version = '1.0.19'
-full_version = '1.0.19.dev0+c0bef7b'
-git_revision = 'c0bef7b4e8bb7b4f73e0d3f0f1af739d464a001a'
+short_version = '1.0.20'
+version = '1.0.20'
+full_version = '1.0.20.dev0+a81ded9'
+git_revision = 'a81ded9e2a8ad0de9f046df79a30870e2d40980c'
 release = False
 
 if not release:
