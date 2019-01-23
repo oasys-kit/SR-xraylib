0b3016d487517593db169392b363bec5db60eae2
lucarebuffi <luca.rebuffi@elettra.eu>
2019-01-23 11:12:27 -0600

    New DABAM interface

--- srxraylib/version.py
+++ srxraylib/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
 short_version = '1.0.21'
 version = '1.0.21'
-full_version = '1.0.21.dev0+bc3c404'
-git_revision = 'bc3c404447be4446f85b519dbd43330bac48c09e'
+full_version = '1.0.21.dev0+40228c9'
+git_revision = '40228c9910edf4bfc56c2caea2456d6a40a383cb'
 release = False
 
 if not release:
