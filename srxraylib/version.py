5f550fd8230a16d84fcfa903584b4d716ea3ff8f
lucarebuffi <lrebuffi@anl.gov>
2019-02-08 14:58:50 -0600

    bug fixing

--- srxraylib/version.py
+++ srxraylib/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
 short_version = '1.0.22'
 version = '1.0.22'
-full_version = '1.0.22.dev0+0b3016d'
-git_revision = '0b3016d487517593db169392b363bec5db60eae2'
+full_version = '1.0.22.dev0+5f43f41'
+git_revision = '5f43f41e5180719395d0da71be17a287e039326f'
 release = False
 
 if not release:
