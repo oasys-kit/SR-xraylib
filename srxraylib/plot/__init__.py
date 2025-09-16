e5f7f3261d287f08d58dd142181805778247139e
lrebuffi <lrebuffi@anl.gov>
2025-09-16 14:38:52 -0500

    Preparation for OASYS 2

--- srxraylib/plot/__init__.py
+++
@@ -1,2 +0,0 @@
-# namespace declaration.
-__import__("pkg_resources").declare_namespace(__name__)
--- srxraylib/sources/__init__.py
+++ srxraylib/sources/__init__.py
@@ -1,2 +1,1 @@
-# namespace declaration.
-__import__("pkg_resources").declare_namespace(__name__)
+
